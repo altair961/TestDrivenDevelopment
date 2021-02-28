24fe34c2459671d8621b79ee10df74507efdd166
altair961 <altair89@list.ru>
2021-02-28 16:43:53 +0100

    First implementation of checkISBN has been added

--- ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -9,16 +9,17 @@ public class ValidateISBNTest {
 	@Test
 	public void checkValidISBN() {
 		ValidateISBN validator = new ValidateISBN();
-		boolean result = validator.checkISBN(140449116);
-		
-		assertTrue(result);
+		boolean result = validator.checkISBN("0140449116");
+		assertTrue("first value", result);
+		result = validator.checkISBN("0140177396");
+		assertTrue("second value", result);
 	}
 	
 	
 	@Test
 	public void checkAnInvalidISBN() {
 		ValidateISBN validator = new ValidateISBN();
-		boolean result = validator.checkISBN(140449117);
+		boolean result = validator.checkISBN("0140449117");
 		
 		assertFalse(result);
 	}
